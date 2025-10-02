1d5713a1418577efe0e3d6b8b4bd5fd31faaaa95
daveroga <daveroga@gmail.com>
2025-10-02 11:07:02 +0200

    remove Hash export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { Eddsa, eddsa, BabyJub, babyJub, Signature, PublicKey, PrivateKey } from
 export { poseidon, Poseidon, OPT } from './poseidon';
 export { Hex } from './hex';
 export { base58FromBytes, base58ToBytes } from './base58';
-export { sha256, Hash } from './sha256';
+export { sha256 } from './sha256';
 export { utils as ffUtils, getRandomBytes } from './ff';
